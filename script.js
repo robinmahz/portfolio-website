deafbe84897bb651999ec42cdb62d984c08845ae
robinmahz <robinmaharjan25@gmail.com>
2024-11-17 17:06:08 +0545

    among us cursore added

--- script.js
+++ script.js
@@ -2,24 +2,37 @@ var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabname, event) {
-    for (tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-    for (tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
 }
 var sidemenu = document.getElementById("sidemenu");
 
 function openmenu() {
-    sidemenu.style.right = "0";
+  sidemenu.style.right = "0";
 }
 function closemenu() {
-    sidemenu.style.right = "-150px";
+  sidemenu.style.right = "-150px";
 }
 window.addEventListener("scroll", function () {
-    var navbar = document.querySelector(".navbar");
-    navbar.classList.toggle("scroll", window.scrollY > 0);
+  var navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("scroll", window.scrollY > 0);
+});
+
+// js for cursor
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.set(".flair", { xPercent: -50, yPercent: -50 });
+
+  let xTo = gsap.quickTo(".flair", "x", { duration: 0.6, ease: "power3" }),
+    yTo = gsap.quickTo(".flair", "y", { duration: 0.6, ease: "power3" });
+
+  window.addEventListener("mousemove", (e) => {
+    xTo(e.clientX);
+    yTo(e.clientY);
+  });
 });
